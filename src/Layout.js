4f4552fae532e3aaf5cb26016533054d8601e9d1
Cresencio Cantu <cresenciocantu@gmail.com>
2020-11-25 08:43:12 -0600

    Update Layout.js
    - move Navigation to SiteHeader

--- src/Layout.js
+++ src/Layout.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Placeholder, VisitorIdentification } from '@sitecore-jss/sitecore-jss-react';
-import { NavLink } from 'react-router-dom';
-import { withTranslation } from 'react-i18next';
 import Helmet from 'react-helmet';
-import { Container, Row, Col } from 'react-bootstrap';
+import SiteHeader from './components/SiteHeader';
 
 // Using bootstrap is completely optional. It's used here to provide a clean layout for samples,
 // without needing extra CSS in the sample app. Remove it in package.json as well if it's removed here.
 import 'bootstrap/dist/css/bootstrap.css';
 import './assets/app.css';
-import logo from './assets/sc_logo.svg';
 
 /*
   APP LAYOUT
@@ -18,39 +15,6 @@ import logo from './assets/sc_logo.svg';
   All routes share this root layout by default (this could be customized in RouteHandler),
   but components added to inner placeholders are route-specific.
 */
-
-// This is boilerplate navigation for sample purposes. Most apps should throw this away and use their own navigation implementation.
-// Most apps may also wish to use GraphQL for their navigation construction; this sample does not simply to support disconnected mode.
-let Navigation = ({ t, i18n }) => (
-  <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-white border-bottom">
-    <h5 className="my-0 mr-md-auto font-weight-normal">
-      <NavLink to="/" className="text-dark">
-        <img src={logo} alt="Sitecore" />
-      </NavLink>
-    </h5>
-    <nav className="my-2 my-md-0 mr-md-3">
-      <a
-        className="p-2 text-dark"
-        href="https://jss.sitecore.net"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {t('Documentation')}
-      </a>
-      <NavLink to="/styleguide" className="p-2 text-dark">
-        {t('Styleguide')}
-      </NavLink>
-      <NavLink to="/graphql" className="p-2 text-dark">
-        {t('GraphQL')}
-      </NavLink>
-    </nav>
-  </div>
-);
-
-// inject dictionary props (`t`) into navigation so we can translate it
-// NOTE: using this is needed instead of using i18next directly to keep
-// the component state updated when i18n state (e.g. current language) changes
-Navigation = withTranslation()(Navigation);
 
 const Layout = ({ route }) => (
   <React.Fragment>
@@ -70,7 +34,7 @@ const Layout = ({ route }) => (
     */}
     <VisitorIdentification />
 
-    <Navigation />
+    <SiteHeader />
 
     {/* root placeholder for the app, which we add components to using route data */}
     <main id="main-content">
