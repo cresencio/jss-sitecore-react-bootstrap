0377c82bd0497db9940136d06e64346fe454bce8
Cresencio Cantu <cresenciocantu@gmail.com>
2020-11-24 22:34:30 -0600

    Update Layout.js
    - remove the margin from the navbar

--- src/Layout.js
+++ src/Layout.js
@@ -22,7 +22,7 @@ import logo from './assets/sc_logo.svg';
 // This is boilerplate navigation for sample purposes. Most apps should throw this away and use their own navigation implementation.
 // Most apps may also wish to use GraphQL for their navigation construction; this sample does not simply to support disconnected mode.
 let Navigation = ({ t, i18n }) => (
-  <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom">
+  <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-white border-bottom">
     <h5 className="my-0 mr-md-auto font-weight-normal">
       <NavLink to="/" className="text-dark">
         <img src={logo} alt="Sitecore" />
